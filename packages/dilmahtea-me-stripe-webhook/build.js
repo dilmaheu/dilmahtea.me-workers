b17ff1d8b8b0a52592ab30b76dfec618ced8e0bf
RafidMuhymin <rafidmuhymin@gmail.com>
2022-10-19 12:51:23 +0600

    fixed node builtins related errors

--- packages/dilmahtea-me-stripe-webhook/build.js
+++ packages/dilmahtea-me-stripe-webhook/build.js
@@ -1,5 +1,11 @@
 import esbuild from 'esbuild'
+import alias from 'esbuild-plugin-alias'
 import { NodeModulesPolyfillPlugin } from '@esbuild-plugins/node-modules-polyfill'
+import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill'
+
+import { createRequire } from 'module'
+
+const require = createRequire(import.meta.url)
 
 const entry = process.argv[2] === '--dev' ? 'src/dev.js' : 'src/prod.js'
 
@@ -9,6 +15,14 @@ esbuild.build({
   minify: false,
   format: 'esm',
   outfile: 'dist/worker.js',
-  plugins: [NodeModulesPolyfillPlugin()],
+  plugins: [
+    // alias 'crypto' to 'crypto-browserify' manually
+    alias({
+      crypto: require.resolve('crypto-browserify'),
+    }),
+    // NodeGlobalsPolyfillPlugin doesn't polyfill crypto due to https://github.com/ionic-team/rollup-plugin-node-polyfills/issues/20
+    NodeModulesPolyfillPlugin(),
+    NodeGlobalsPolyfillPlugin({ buffer: true }),
+  ],
   logLevel: 'info',
 })
