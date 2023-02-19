d0307245e469dedda1fb867051c094ceeef3b31a
RafidMuhymin <rafidmuhymin@gmail.com>
2023-02-19 23:16:07 +0600

    fixed SizeName is not defined error

--- src/workers/dilmahtea-me-strapi-webhook/src/utils/updateProductsStore.js
+++ src/workers/dilmahtea-me-strapi-webhook/src/utils/updateProductsStore.js
@@ -128,7 +128,7 @@ export async function updateProductsStore(model, reply) {
           return (
             productLocale === locale &&
             productVariantName === VariantName &&
-            productSize === SizeName
+            productSize === Size
           );
         });
 
