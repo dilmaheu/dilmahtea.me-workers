f83aaf904c2cf11c40f776fc9e94f1a398d5d839
RafidMuhymin <rafidmuhymin@gmail.com>
2023-03-16 01:37:16 +0600

    moved to native cloudflare workers stripe sdk

--- src/workers/dilmahtea-me-stripe/src/dev.js
+++ src/workers/dilmahtea-me-stripe/src/dev.js
@@ -1,28 +1,27 @@
-import Stripe from 'stripe';
+import Stripe from "stripe";
+import { getValidatedData } from "./utils/getValidatedData";
 
 const stripe = new Stripe(STRIPE_DEVELOPMENT_SECRET_KEY_CODE, {
   // Cloudflare Workers use the Fetch API for their API requests.
   httpClient: Stripe.createFetchHttpClient(),
-  apiVersion: '2020-08-27',
+  apiVersion: "2020-08-27",
 });
 
 const headers = new Headers({
-  'Content-Type': 'application/json',
-  'Access-Control-Allow-Origin': '*',
-  'Access-Control-Allow-Headers': '*',
-  'Access-Control-Allow-Methods': 'OPTIONS, POST',
-  'Access-Control-Max-Age': '-1',
+  "Content-Type": "application/json",
+  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Headers": "*",
+  "Access-Control-Allow-Methods": "OPTIONS, POST",
+  "Access-Control-Max-Age": "-1",
 });
 
 const reply = (message, status) => {
   return new Response(message, { status, headers });
 };
 
-const handlePOST = async request => {
+const handlePOST = async (request) => {
   const body = await request.formData(),
     data = Object.fromEntries(body);
-
-  const { getValidatedData } = await import('./utils/getValidatedData.js');
 
   const validatedData = await getValidatedData(data);
 
@@ -60,32 +59,32 @@ const handlePOST = async request => {
 
   const searchParams = new URLSearchParams();
 
-  searchParams.set('first_name', first_name);
-  searchParams.set('last_name', last_name);
-  searchParams.set('email', email);
-  searchParams.set('favorite_tea', favorite_tea);
-  searchParams.set('country', country);
-  searchParams.set('city', city);
-  searchParams.set('street', street);
-  searchParams.set('postal_code', postal_code);
+  searchParams.set("first_name", first_name);
+  searchParams.set("last_name", last_name);
+  searchParams.set("email", email);
+  searchParams.set("favorite_tea", favorite_tea);
+  searchParams.set("country", country);
+  searchParams.set("city", city);
+  searchParams.set("street", street);
+  searchParams.set("postal_code", postal_code);
 
   const queryString = searchParams.toString(),
-    cancel_url = origin_url + '?' + queryString;
+    cancel_url = origin_url + "?" + queryString;
 
   try {
     // Create new Checkout Session for the order.
     // Redirects the customer to s Stripe checkout page.
     // @see https://stripe.com/docs/payments/accept-a-payment?integration=checkout
     const session = await stripe.checkout.sessions.create({
-      payment_method_types: ['card', 'ideal'],
-      mode: 'payment',
+      payment_method_types: ["card", "ideal"],
+      mode: "payment",
       customer_email: email,
       locale: locale,
       line_items: [
         {
           quantity: 1,
           price_data: {
-            currency: 'eur',
+            currency: "eur",
             unit_amount: Math.round(price * 100),
             product_data: {
               name: product_name,
@@ -101,11 +100,11 @@ const handlePOST = async request => {
     const paymentIntentID = session.payment_intent;
 
     switch (payment_type) {
-      case 'crowdfunding':
+      case "crowdfunding":
         await CROWDFUNDINGS.put(paymentIntentID, paymentData);
         break;
 
-      case 'ecommerce':
+      case "ecommerce":
         await ECOMMERCE_PAYMENTS.put(paymentIntentID, paymentData);
         break;
     }
@@ -116,11 +115,11 @@ const handlePOST = async request => {
   }
 };
 
-const handleOPTIONS = request => {
+const handleOPTIONS = (request) => {
   if (
-    request.headers.get('Origin') !== null &&
-    request.headers.get('Access-Control-Request-Method') !== null &&
-    request.headers.get('Access-Control-Request-Headers') !== null
+    request.headers.get("Origin") !== null &&
+    request.headers.get("Access-Control-Request-Method") !== null &&
+    request.headers.get("Access-Control-Request-Headers") !== null
   ) {
     // Handle CORS pre-flight request.
     return new Response(null, {
@@ -130,27 +129,27 @@ const handleOPTIONS = request => {
     // Handle standard OPTIONS request.
     return new Response(null, {
       headers: {
-        Allow: 'POST, OPTIONS',
+        Allow: "POST, OPTIONS",
       },
     });
   }
 };
 
-addEventListener('fetch', event => {
+addEventListener("fetch", (event) => {
   const { request } = event;
 
   let { pathname: urlPathname } = new URL(request.url);
 
-  if (urlPathname === '/') {
+  if (urlPathname === "/") {
     switch (request.method) {
-      case 'POST':
+      case "POST":
         return event.respondWith(handlePOST(request));
-      case 'OPTIONS':
+      case "OPTIONS":
         return event.respondWith(handleOPTIONS(request));
     }
   }
 
   return event.respondWith(
-    reply(JSON.stringify({ error: `Method or Path Not Allowed` }), 405),
+    reply(JSON.stringify({ error: `Method or Path Not Allowed` }), 405)
   );
 });
--- src/workers/dilmahtea-me-stripe/src/utils/getValidatedData.js
+++ src/workers/dilmahtea-me-stripe/src/utils/getValidatedData.js
@@ -1,4 +1,4 @@
-import Validator from '@chantouchsek/validatorjs';
+import Validator from "@chantouchsek/validatorjs";
 
 const query = `
   {
@@ -75,127 +75,129 @@ const query = `
   }
 `;
 
-// process data for validation
-const CMSData = await fetch(CMS_GRAPHQL_ENDPOINT, {
-  method: 'POST',
-  headers: {
-    'Content-Type': 'application/json',
-    Authorization: `Bearer ${CMS_ACCESS_TOKEN}`,
-  },
-  body: JSON.stringify({ query }),
-}).then(response => response.json());
-
-const {
-  data: {
-    crowdfundingPlans: { data: crowdfundingPlans },
-    recurringElement: { data: recurringElement },
-    i18NLocales: { data: i18NLocales },
-    products: { data: productsData },
-  },
-} = CMSData;
-
-const crowdfundingPerks = {};
-
-crowdfundingPlans.forEach(({ attributes: { Perk, Price_EUR_excl_VAT } }) => {
-  crowdfundingPerks[Perk] = Price_EUR_excl_VAT;
-});
-
-const locales = i18NLocales.map(({ attributes: { code } }) =>
-  code.substring(0, 2),
-);
-
-const products = [];
-
-productsData.forEach(({ attributes }) => {
-  const { SKU, Price, Weight_tea, Weight_tea_unit, localizations } = attributes;
-
-  const names = {};
-
-  [{ attributes }, ...localizations.data].forEach(
-    ({ attributes: { locale, Title } }) => {
-      names[locale.substring(0, 2)] = Title;
-    },
-  );
-
-  const size = Weight_tea + Weight_tea_unit;
-
-  products[SKU] = {
-    sku: SKU,
-    names,
-    price: Price,
-    size,
-  };
-});
-
-const countries = CMSData.data.countries.data.map(
-    ({ attributes: { name } }) => name,
-  ),
-  kindnessCauses = CMSData.data.kindnessCauses.data.map(
-    ({ attributes: { cause } }) => cause,
-  );
-
-const shippingMethods = {};
-
-CMSData.data.shippingMethods.data.forEach(
-  ({ attributes: { method, cost } }) => {
-    shippingMethods[method] = cost;
-  },
-);
-
-const companyName = recurringElement.attributes.Company_name;
-
-const paymentTypes = ['ecommerce', 'crowdfunding'];
-
-Validator.register(
-  'cart',
-  function(cart) {
-    if (!cart) return false;
-
-    cart = JSON.parse(cart);
-
-    for (const id in cart) {
-      const productData = products[id];
-
-      if (!productData) return false;
-
-      const { sku, names, price, size } = productData,
-        tax = Math.round(price * 9) / 100,
-        {
-          sku: cartProductSKU,
-          tax: cartProductTax,
-          size: cartProductSize,
-          price: cartProductPrice,
-          quantity: cartProductQuantity,
-        } = cart[id];
-
-      const cartProductNames = JSON.parse(cart[id].names);
-
-      if (cartProductQuantity <= 0 || !Number.isInteger(cartProductQuantity))
-        return false;
-
-      const taxIncludedPrice = (price + tax) * cartProductQuantity,
-        totalTax = Math.round(tax * cartProductQuantity * 100) / 100;
-
-      if (
-        cartProductSKU !== sku ||
-        cartProductSize !== size ||
-        cartProductTax !== totalTax ||
-        cartProductPrice !== taxIncludedPrice
-      )
-        return false;
-
-      for (const locale in cartProductNames) {
-        if (cartProductNames[locale] !== names[locale]) return false;
-      }
-    }
-
-    return true;
-  },
-  'Invalid cart data.',
-);
-
 export async function getValidatedData(data) {
-  const isCrowdfundingPayment = data.payment_type === 'crowdfunding';
+  // process data for validation
+  const CMSData = await fetch(CMS_GRAPHQL_ENDPOINT, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${CMS_ACCESS_TOKEN}`,
+    },
+    body: JSON.stringify({ query }),
+  }).then((response) => response.json());
+
+  const {
+    data: {
+      crowdfundingPlans: { data: crowdfundingPlans },
+      recurringElement: { data: recurringElement },
+      i18NLocales: { data: i18NLocales },
+      products: { data: productsData },
+    },
+  } = CMSData;
+
+  const crowdfundingPerks = {};
+
+  crowdfundingPlans.forEach(({ attributes: { Perk, Price_EUR_excl_VAT } }) => {
+    crowdfundingPerks[Perk] = Price_EUR_excl_VAT;
+  });
+
+  const locales = i18NLocales.map(({ attributes: { code } }) =>
+    code.substring(0, 2)
+  );
+
+  const products = [];
+
+  productsData.forEach(({ attributes }) => {
+    const { SKU, Price, Weight_tea, Weight_tea_unit, localizations } =
+      attributes;
+
+    const names = {};
+
+    [{ attributes }, ...localizations.data].forEach(
+      ({ attributes: { locale, Title } }) => {
+        names[locale.substring(0, 2)] = Title;
+      }
+    );
+
+    const size = Weight_tea + Weight_tea_unit;
+
+    products[SKU] = {
+      sku: SKU,
+      names,
+      price: Price,
+      size,
+    };
+  });
+
+  const countries = CMSData.data.countries.data.map(
+      ({ attributes: { name } }) => name
+    ),
+    kindnessCauses = CMSData.data.kindnessCauses.data.map(
+      ({ attributes: { cause } }) => cause
+    );
+
+  const shippingMethods = {};
+
+  CMSData.data.shippingMethods.data.forEach(
+    ({ attributes: { method, cost } }) => {
+      shippingMethods[method] = cost;
+    }
+  );
+
+  const companyName = recurringElement.attributes.Company_name;
+
+  const paymentTypes = ["ecommerce", "crowdfunding"];
+
+  Validator.register(
+    "cart",
+    function (cart) {
+      if (!cart) return false;
+
+      cart = JSON.parse(cart);
+
+      for (const id in cart) {
+        const productData = products[id];
+
+        if (!productData) return false;
+
+        const { sku, names, price, size } = productData,
+          tax = Math.round(price * 9) / 100,
+          {
+            sku: cartProductSKU,
+            tax: cartProductTax,
+            size: cartProductSize,
+            price: cartProductPrice,
+            quantity: cartProductQuantity,
+          } = cart[id];
+
+        const cartProductNames = JSON.parse(cart[id].names);
+
+        if (cartProductQuantity <= 0 || !Number.isInteger(cartProductQuantity))
+          return false;
+
+        const taxIncludedPrice = (price + tax) * cartProductQuantity,
+          totalTax = Math.round(tax * cartProductQuantity * 100) / 100;
+
+        if (
+          cartProductSKU !== sku ||
+          cartProductSize !== size ||
+          cartProductTax !== totalTax ||
+          cartProductPrice !== taxIncludedPrice
+        )
+          return false;
+
+        for (const locale in cartProductNames) {
+          if (cartProductNames[locale] !== names[locale]) return false;
+        }
+      }
+
+      return true;
+    },
+    "Invalid cart data."
+  );
+
+  // validate data
+  const isCrowdfundingPayment = data.payment_type === "crowdfunding";
 
   data.tax &&= +data.tax;
   data.price = +data.price;
@@ -215,14 +217,14 @@ export async function getValidatedData(data) {
 
     product_desc = Object.values(cart)
       .map(({ names, quantity }) => `${quantity}x ${JSON.parse(names)[locale]}`)
-      .join(', ');
+      .join(", ");
 
     [tax, price] = Object.values(cart).reduce(
       ([totalTax, totalPrice], { tax, price }) => [
         totalTax + tax,
         totalPrice + price,
       ],
-      [0, 0],
+      [0, 0]
     );
 
     tax = Math.round(tax * 100) / 100;
@@ -232,48 +234,48 @@ export async function getValidatedData(data) {
   }
 
   const validator = new Validator(data, {
-    first_name: 'required|string',
-    last_name: 'required|string',
-    email: 'required|email',
+    first_name: "required|string",
+    last_name: "required|string",
+    email: "required|email",
     favorite_tea: [
-      'required_if:payment_type,crowdfunding',
-      isCrowdfundingPayment ? 'string' : { in: [] },
+      "required_if:payment_type,crowdfunding",
+      isCrowdfundingPayment ? "string" : { in: [] },
     ],
     country: [
-      'required',
-      { in: isCrowdfundingPayment ? 'Netherlands' : countries },
+      "required",
+      { in: isCrowdfundingPayment ? "Netherlands" : countries },
     ],
-    city: 'required|string',
-    street: 'required|string',
-    postal_code: 'required|regex:/^[\\w- ]+$/',
+    city: "required|string",
+    street: "required|string",
+    postal_code: "required|regex:/^[\\w- ]+$/",
     kindness_cause: [
-      'required_if:payment_type,ecommerce',
+      "required_if:payment_type,ecommerce",
       { in: isCrowdfundingPayment ? [] : kindnessCauses },
     ],
     shipping_method: [
-      'required_if:payment_type,ecommerce',
+      "required_if:payment_type,ecommerce",
       { in: isCrowdfundingPayment ? [] : Object.keys(shippingMethods) },
     ],
     shipping_cost: [
-      'required_if:payment_type,ecommerce',
+      "required_if:payment_type,ecommerce",
       { in: isCrowdfundingPayment ? [] : [+shippingMethods[shipping_method]] },
     ],
     perk: [
-      'required_if:payment_type,crowdfunding',
+      "required_if:payment_type,crowdfunding",
       { in: isCrowdfundingPayment ? Object.keys(crowdfundingPerks) : [] },
     ],
     cart: [
-      'required_if:payment_type,ecommerce',
-      isCrowdfundingPayment ? { in: [] } : 'cart',
+      "required_if:payment_type,ecommerce",
+      isCrowdfundingPayment ? { in: [] } : "cart",
     ],
-    product_name: ['required', { in: [companyName] }],
-    product_desc: ['required', { in: [product_desc] }],
-    price: ['required', { in: [total] }],
-    tax: ['required_if:payment_type,ecommerce', { in: [tax] }],
-    payment_type: ['required', { in: paymentTypes }],
-    locale: ['required', { in: locales }],
-    origin_url: 'required|url',
-    success_url: 'required|url',
+    product_name: ["required", { in: [companyName] }],
+    product_desc: ["required", { in: [product_desc] }],
+    price: ["required", { in: [total] }],
+    tax: ["required_if:payment_type,ecommerce", { in: [tax] }],
+    payment_type: ["required", { in: paymentTypes }],
+    locale: ["required", { in: locales }],
+    origin_url: "required|url",
+    success_url: "required|url",
   });
 
   try {
