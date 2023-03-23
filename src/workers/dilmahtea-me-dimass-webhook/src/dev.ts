2b167522a927900cc4c961ee56b734ed522f7c95
yaikohi <erik.beem@hotmail.com>
2023-03-23 14:12:44 +0100

    WIP: just returns the request if its a POST for now

--- src/workers/dilmahtea-me-dimass-webhook/src/dev.ts
+++ src/workers/dilmahtea-me-dimass-webhook/src/dev.ts
@@ -28,64 +28,81 @@ export default {
   ): Promise<Response> {
     // SETUP
     // 1, 4, 7, 8, 128 are valid `product id`'s
-    const url = "https://cms.dilmahtea.me/api/products/1";
+    // const url = "https://cms.dilmahtea.me/api/products/1";
 
-    if (request.method === 'POST') {
-      // handle post request
+    // if (request.method === "POST") {
+    //   return new Response(JSON.stringify(await request.json(), null, 2), {
+    //     headers: {
+    //       "content-type": "application/json;charset=UTF-8",
+    //     },
+    //   });
+    // }
+
+
+    if (request.method === "POST") {
+      return new Response(JSON.stringify(await request.json(), null, 2), {
+        headers: {
+          "content-type": "application/json;charset=UTF-8",
+        },
+      });
     }
 
-    if (request.headers) {
-      console.log(request.headers)
-    }
-    
-    const headers = new Headers({
-      Accept: "*/*",
-      Authorization: `Bearer ${env.STRAPI_APIKEY}`,
-      "Content-Type": "application/json",
-      "User-Agent": "cloudflare-worker",
-    });
+    return new Response(
+      JSON.stringify({ message: "No POST request received." }, null, 2),
+      {
+        headers: {
+          "content-type": "application/json;charset=UTF-8",
+        },
+      }
+    );
+    // const headers = new Headers({
+    //   Accept: "*/*",
+    //   Authorization: `Bearer ${env.STRAPI_APIKEY}`,
+    //   "Content-Type": "application/json",
+    //   "User-Agent": "cloudflare-worker",
+    // });
 
-    const requestOptionsGet = {
-      method: "GET",
-      headers,
-    };
+    // const requestOptionsGet = {
+    //   method: "GET",
+    //   headers,
+    // };
 
-    const responseGet = await fetch(url, requestOptionsGet);
+    // const responseGet = await fetch(url, requestOptionsGet);
 
-    if (!responseGet.ok) {
-      throw new Error(`PUT request failed; ${responseGet}`);
-    }
+    // if (!responseGet.ok) {
+    //   throw new Error(`PUT request failed; ${responseGet}`);
+    // }
 
-    const rawResultsGet: Main = await responseGet.json();
+    // const rawResultsGet: Main = await responseGet.json();
 
-    const body = {
-      data: { Stock_amount: "2" },
-    };
+    // const body = {
+    //   data: { Stock_amount: "2" },
+    // };
 
-    const requestOptionsPUT = {
-      /**
-       * Strapi doesn't support PATCH, so I need to fetch the data
-       * and the only update the fields I want to update,
-       * then send a PUT request to update all the fields.
-       */
-      method: "PUT",
-      body: JSON.stringify(body, null, 2),
-      headers,
-    };
+    // const requestOptionsPUT = {
+    //   /**
+    //    * Strapi doesn't support PATCH, so I need to fetch the data
+    //    * and the only update the fields I want to update,
+    //    * then send a PUT request to update all the fields.
+    //    */
+    //   method: "PUT",
+    //   body: JSON.stringify(body, null, 2),
+    //   headers,
+    // };
 
-    const responsePut = await fetch(url, requestOptionsPUT);
+    // const responsePut = await fetch(url, requestOptionsPUT);
 
-    if (!responsePut.ok) {
-      console.table(responsePut);
-      throw new Error(`PUT request failed; ${responseGet}`);
-    }
-    const rawResultsPut = await responsePut.json();
-    const resultsPut = JSON.stringify(rawResultsPut, null, 2);
-    console.log(rawResultsPut);
-    return new Response(resultsPut, {
-      headers: {
-        "content-type": "application/json;charset=UTF-8",
-      },
-    });
+    // if (!responsePut.ok) {
+    //   console.table(responsePut);
+    //   throw new Error(`PUT request failed; ${responseGet}`);
+    // }
+    // const rawResultsPut = await responsePut.json();
+    // const resultsPut = JSON.stringify(rawResultsPut, null, 2);
+    // console.log(rawResultsPut);
+    // return new Response(resultsPut, {
+    //   headers: {
+    //     "content-type": "application/json;charset=UTF-8",
+    //   },
+    // });
   },
 };
