1651fb5b8d7e17138c3c7b458ee53a49f8fccaec
yaikohi <erik.beem@hotmail.com>
2023-03-28 13:15:41 +0200

    fix: remove unused file

--- src/workers/dilmahtea-me-dimass-webhook/src/utils/handle-post.ts
+++
@@ -1,9 +0,0 @@
-export default async function (request: Request) {
-  const data = await request.json();
-
-  console.log(data);
-  return new Response(JSON.stringify(data, null, 2), {
-    status: 200,
-    statusText: "Something went right?",
-  });
-}
