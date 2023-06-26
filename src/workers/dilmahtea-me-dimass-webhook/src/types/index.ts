56dfcd5bf01aad291b7a69f51946e563db6a4b2e
RafidMuhymin <rafidmuhymin@gmail.com>
2023-06-27 04:03:32 +0600

    renamed dimass worker files & folders

--- src/workers/dilmahtea-me-dimass-webhook/src/types/index.ts
+++ src/workers/dilmahtea-me-dimass-webhook/src/types/index.ts
@@ -1,6 +1,6 @@
 import type { ENV } from "./kv";
-import type { WebhookResponseData } from "./dimass-webhook";
-import type { Item, GetDimassStockResponse } from "./dimass-webhook-get-stock-response";
+import type { WebhookResponseData } from "./dimassWebhook";
+import type { Item, GetDimassStockResponse } from "./dimassWebhookGetStockResponse";
 import type { StrapiResponseProducts, StrapiResponseProduct } from "./strapi";
 
 export { ENV, WebhookResponseData, Item, GetDimassStockResponse, StrapiResponseProducts, StrapiResponseProduct };
