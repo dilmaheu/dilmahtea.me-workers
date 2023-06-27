38fc81e49735b9a4355974e76702781a3092fe7a
RafidMuhymin <rafidmuhymin@gmail.com>
2023-06-27 16:33:43 +0600

    create order automatically on payment

---
+++ src/workers/dilmahtea-me-stripe-webhook/src/utils/createPurchaseEvent.js
@@ -0,0 +1,35 @@
+// @ts-check
+
+export default function createPurchaseEvent({
+  promises,
+  origin_url,
+  paymentIntentData,
+}) {
+  const { cart, request_headers } = paymentIntentData,
+    purchasedProducts = Object.values(cart),
+    purchaseEventRequestHeaders = new Headers(request_headers);
+
+  purchaseEventRequestHeaders.set("Content-Type", "application/json");
+
+  purchasedProducts.forEach((product) => {
+    promises.push(
+      fetch("https://plausible.io/api/event", {
+        method: "POST",
+        headers: Object.fromEntries(purchaseEventRequestHeaders),
+        body: JSON.stringify({
+          name: "Purchase",
+          url: origin_url,
+          domain: "dilmahtea.me",
+          props: {
+            SKU: product.sku,
+            Title: JSON.parse(product.names).en,
+            Currency: "EUR",
+            Price: product.price,
+            Quantity: product.quantity,
+            Category: "Tea",
+          },
+        }),
+      })
+    );
+  });
+}
