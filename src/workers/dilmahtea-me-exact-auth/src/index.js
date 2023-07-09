123d8b7d5a3045b5e65f4fd2ce815cd317411200
RafidMuhymin <rafidmuhymin@gmail.com>
2023-07-09 16:03:59 +0600

    added exact auth worker

---
+++ src/workers/dilmahtea-me-exact-auth/src/index.js
@@ -0,0 +1,33 @@
+export default {
+  async scheduled(_, env, ctx) {
+    ctx.waitUntil(
+      new Promise(async (resolve, reject) => {
+        const refresh_token = await env.EXACT_TOKENS.get("REFRESH_TOKEN");
+
+        const data = {
+          refresh_token,
+          grant_type: "refresh_token",
+          client_id: env.CLIENT_ID,
+          client_secret: env.CLIENT_SECRET,
+        };
+
+        const response = await fetch(env.EXACT_OAUTH_TOKEN_API_ENDPOINT, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/x-www-form-urlencoded;",
+          },
+          body: new URLSearchParams(data).toString(),
+        }).then((res) => res.json());
+
+        if (response.error) {
+          reject(response.error + " | " + response.error_description);
+        }
+
+        await env.EXACT_TOKENS.put("ACCESS_TOKEN", response.access_token);
+        await env.EXACT_TOKENS.put("REFRESH_TOKEN", response.refresh_token);
+
+        resolve();
+      })
+    );
+  },
+};
