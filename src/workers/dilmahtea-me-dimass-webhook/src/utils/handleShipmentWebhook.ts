b1624da3e6811f2dfd7b1ab944af1827b5b35a96
RafidMuhymin <rafidmuhymin@gmail.com>
2023-07-14 05:33:49 +0600

    refactored shipment webhook data var name

--- src/workers/dilmahtea-me-dimass-webhook/src/utils/handleShipmentWebhook.ts
+++ src/workers/dilmahtea-me-dimass-webhook/src/utils/handleShipmentWebhook.ts
@@ -6,11 +6,11 @@ import fetchExactAPIConstructor from "../../../../utils/fetchExactAPIConstructor
 
 export default async function handleShipmentWebhook(
   env: ENV,
-  webhookData: Shipment
+  shipment: Shipment
 ) {
   const fetchExactAPI = fetchExactAPIConstructor(env);
 
-  const orderNumber = +webhookData.order.order_number;
+  const orderNumber = +shipment.order.order_number;
 
   if (Number.isNaN(orderNumber) || orderNumber < 20000) {
     return reply({ success: null, message: "Irrelevant order" }, 200);
@@ -23,11 +23,11 @@ export default async function handleShipmentWebhook(
 
   const orderID = salesOrder.feed.entry.content["m:properties"]["d:OrderID"];
 
-  if (webhookData.state === 15 && webhookData.sub_state === null) {
+  if (shipment.state === 15 && shipment.sub_state === null) {
     await sendInvoice(orderID, orderNumber, fetchExactAPI, env);
 
     return reply({ success: true, message: "Sales invoice sent" }, 200);
-  } else if (webhookData.state === 15 && webhookData.sub_state === 1530) {
+  } else if (shipment.state === 15 && shipment.sub_state === 1530) {
     // update delivery status
   } else {
     return reply({ success: false, message: "Invalid shipment state" }, 400);
