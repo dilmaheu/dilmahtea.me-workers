93b3dd7b987af6ae67c6e72945ee143bba630f46
RafidMuhymin <rafidmuhymin@gmail.com>
2023-07-16 23:41:04 +0600

    added shipping method & description to invoice

--- src/workers/dilmahtea-me-dimass-webhook/src/utils/handleShipmentWebhook.ts
+++ src/workers/dilmahtea-me-dimass-webhook/src/utils/handleShipmentWebhook.ts
@@ -1,7 +1,9 @@
 import { ENV, Shipment } from "../types";
 
 import { reply } from "../../../../utils/createModuleWorker";
+
 import sendInvoice from "./sendInvoice";
+import createGoodsDelivery from "./createGoodsDelivery";
 import fetchExactAPIConstructor from "../../../../utils/fetchExactAPIConstructor";
 
 export default async function handleShipmentWebhook(
@@ -16,20 +18,46 @@ export default async function handleShipmentWebhook(
     return reply({ success: null, message: "Irrelevant order" }, 200);
   }
 
-  const salesOrder = await fetchExactAPI(
-    "GET",
-    `/salesorder/SalesOrders?$filter=OrderNumber eq ${orderNumber}&$select=OrderID`
-  );
+  const { state, sub_state } = shipment;
 
-  const orderID = salesOrder.feed.entry.content["m:properties"]["d:OrderID"];
+  if (state === 15 && [null, 1530].includes(sub_state)) {
+    const salesOrder = await fetchExactAPI(
+      "GET",
+      `/salesorder/SalesOrders?$filter=OrderNumber eq ${orderNumber}&$select=OrderID,SalesOrderLines`
+    );
 
-  if (shipment.state === 15 && shipment.sub_state === null) {
-    await sendInvoice(orderID, orderNumber, fetchExactAPI, env);
+    const orderID = salesOrder.feed.entry.content["m:properties"]["d:OrderID"];
 
-    return reply({ success: true, message: "Sales invoice sent" }, 200);
-  } else if (shipment.state === 15 && shipment.sub_state === 1530) {
-    // update delivery status
-  } else {
-    return reply({ success: false, message: "Invalid shipment state" }, 400);
+    console.log(
+      salesOrder.feed.entry.content["m:properties"]["d:SalesOrderLines"]
+    );
+
+    const [shipment_colli] = shipment.shipment_colli,
+      { tracking_url, courier_code } = shipment_colli;
+
+    const shippingMethod = await fetchExactAPI(
+      "GET",
+      `/sales/ShippingMethods?$filter=Code eq '${courier_code}'&$select=ID`
+    );
+
+    const shippingMethodID =
+      shippingMethod.feed.entry.content["m:properties"]["d:ID"];
+
+    if (sub_state === null) {
+      await sendInvoice(
+        orderID,
+        orderNumber,
+        tracking_url,
+        shippingMethodID,
+        fetchExactAPI,
+        env
+      );
+
+      return reply({ success: true, message: "Sales invoice sent" }, 200);
+    } else {
+      // update delivery status
+    }
   }
+
+  return reply({ success: false, message: "Invalid shipment state" }, 400);
 }
--- src/workers/dilmahtea-me-dimass-webhook/src/utils/sendInvoice.ts
+++ src/workers/dilmahtea-me-dimass-webhook/src/utils/sendInvoice.ts
@@ -1,13 +1,17 @@
 export default async function sendInvoice(
   orderID,
   orderNumber,
+  tracking_url,
+  shippingMethodID,
   fetchExactAPI,
   env
 ) {
   await fetchExactAPI("POST", "/salesinvoice/InvoiceSalesOrders", {
     CreateMode: 1,
     InvoiceMode: 1,
+    Description: tracking_url,
     JournalCode: env.JOURNAL_CODE,
+    ShippingMethod: shippingMethodID,
     SalesOrderIDs: [{ ID: orderID }],
   });
 
@@ -21,10 +25,20 @@ export default async function sendInvoice(
   const { "d:InvoiceID": invoiceID } =
     invoice.feed.entry.content["m:properties"];
 
-  await fetchExactAPI("POST", "/salesinvoice/PrintedSalesInvoices", {
-    InvoiceID: invoiceID,
-    SendEmailToCustomer: true,
-  });
+  await Promise.all([
+    fetchExactAPI("PUT", `/salesinvoice/SalesInvoices(guid'${invoiceID}')`, {
+      Description: tracking_url,
+      ShippingMethod: shippingMethodID,
+    }).then(() =>
+      fetchExactAPI("POST", "/salesinvoice/PrintedSalesInvoices", {
+        InvoiceID: invoiceID,
+        SendEmailToCustomer: true,
+      })
+    ),
+    fetchExactAPI("PUT", `/salesorder/SalesOrders(guid'${orderID}')`, {
+      ShippingMethod: shippingMethodID,
+    }),
+  ]);
 
   console.log("Exact: Sales invoice sent successfully");
 }
