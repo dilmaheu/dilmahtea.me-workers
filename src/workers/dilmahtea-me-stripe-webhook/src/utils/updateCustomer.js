1a60566fc15c6744b384938a7c3e0f684381995a
RafidMuhymin <rafidmuhymin@gmail.com>
2023-07-17 06:27:53 +0600

    fixed used can't select an old address

--- src/workers/dilmahtea-me-stripe-webhook/src/utils/updateCustomer.js
+++ src/workers/dilmahtea-me-stripe-webhook/src/utils/updateCustomer.js
@@ -49,27 +49,48 @@ export default async function updateCustomer(
   promises.push(
     fetchExactAPI(
       "GET",
-      `/CRM/Addresses?$filter=Account eq guid'${Customer["d:ID"]}'&$select=Type,City,Country,Postcode,AddressLine1,AddressLine2,AddressLine3`
+      `/CRM/Addresses?$filter=Account eq guid'${Customer["d:ID"]}'&$select=ID,Main,Type,City,Country,Postcode,AddressLine1,AddressLine2,AddressLine3`
     )
       .then(({ feed: { entry } }) => (Array.isArray(entry) ? entry : [entry]))
       .then(async (linkedAddresses) =>
         Promise.all(
           Object.values(AddressTypes).map(async (Type) => {
+            let matchedAddress;
+
             const shouldCreateNewAddress = linkedAddresses.every(
               ({ content: { "m:properties": props } }) =>
                 props["d:Type"] !== Type ||
-                JSON.stringify({ Type, ...Address }) !==
-                  JSON.stringify({
-                    Type: props["d:Type"],
-                    AddressLine1: props["d:AddressLine1"],
-                    // remove empty string address lines
-                    AddressLine2: props["d:AddressLine2"] || undefined,
-                    AddressLine3: props["d:AddressLine3"] || undefined,
-                    City: props["d:City"],
-                    Country: props["d:Country"],
-                    Postcode: props["d:Postcode"],
-                  })
+                (() => {
+                  const isCurrentAddress =
+                    JSON.stringify({ Type, ...Address }) ===
+                    JSON.stringify({
+                      Type: props["d:Type"],
+                      AddressLine1: props["d:AddressLine1"],
+                      // remove empty string address lines
+                      AddressLine2: props["d:AddressLine2"] || undefined,
+                      AddressLine3: props["d:AddressLine3"] || undefined,
+                      City: props["d:City"],
+                      Country: props["d:Country"],
+                      Postcode: props["d:Postcode"],
+                    });
+
+                  if (isCurrentAddress) {
+                    matchedAddress = props;
+                  }
+
+                  return !isCurrentAddress;
+                })()
             );
+
+            if (matchedAddress && !matchedAddress["d:Main"]) {
+              await fetchExactAPI(
+                "PUT",
+                `/CRM/Addresses(guid'${matchedAddress["d:ID"]}')`,
+                {
+                  Main: true,
+                }
+              );
+            }
 
             if (shouldCreateNewAddress) {
               await fetchExactAPI("POST", "/CRM/Addresses", {
