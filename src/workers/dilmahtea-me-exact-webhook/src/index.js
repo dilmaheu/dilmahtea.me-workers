9b5be544e68437f96ff4706a29b2f1ffd5333ad1
RafidMuhymin <rafidmuhymin@gmail.com>
2023-08-04 16:08:13 +0600

    added validateSignture shared util

--- src/workers/dilmahtea-me-exact-webhook/src/index.js
+++ src/workers/dilmahtea-me-exact-webhook/src/index.js
@@ -1,13 +1,21 @@
 // @ts-check
 
-import validateSignature from "./utils/validateSignature";
 import updateProductPrice from "./utils/updateProductPrice";
+
+import validateSignature from "../../../utils/validateSignature";
 import createModuleWorker from "../../../utils/createModuleWorker";
 import fetchExactAPIConstructor from "../../../utils/fetchExactAPIConstructor";
 
 async function handlePOST(request, env) {
   const webhookData = await request.json(),
-    Content = await validateSignature(webhookData, env);
+    { Content, HashCode } = webhookData;
+
+  await validateSignature(
+    Content,
+    "SHA-256",
+    HashCode.toLowerCase(),
+    env.EXACT_WEBHOOK_SECRET
+  );
 
   const fetchExactAPI = fetchExactAPIConstructor(env);
 
