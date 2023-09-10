00c2ca8344a2e751a580ad92e7de9d9ad736debe
RafidMuhymin <rafidmuhymin@gmail.com>
2023-09-11 02:51:16 +0600

    optimized goods delivery creation logic

--- src/workers/dilmahtea-me-dimass-webhook/src/utils/createGoodsDelivery.ts
+++ src/workers/dilmahtea-me-dimass-webhook/src/utils/createGoodsDelivery.ts
@@ -5,31 +5,31 @@ export default async function createGoodsDelivery(
   shippingMethodID,
   fetchExactAPI,
 ) {
-  const [orderLines, { currentLocalTime: currentAmsterdamTime }] =
-    await Promise.all([
-      fetchExactAPI(
-        "GET",
-        `/salesorder/SalesOrderLines?$filter=OrderNumber eq ${orderNumber}`,
-      ),
-      fetch(
-        "https://timeapi.io/api/TimeZone/zone?timeZone=Europe/Amsterdam",
-      ).then(async (res) => res.json()) as Promise<Record<string, any>>,
-    ]);
+  const [orderLines, AmsterdamTime] = await Promise.all([
+    fetchExactAPI(
+      "GET",
+      `/salesorder/SalesOrderLines?$filter=OrderNumber eq ${orderNumber}`,
+    ),
+    fetch(
+      "https://timeapi.io/api/TimeZone/zone?timeZone=Europe/Amsterdam",
+    ).then(async (res) => res.json()) as Promise<Record<string, any>>,
+  ]);
 
-  const orderLinesEntries = [orderLines.feed.entry].flat();
+  const DeliveryDate = AmsterdamTime.currentLocalTime,
+    GoodsDeliveryLines = [orderLines.feed.entry].flat().map((orderLine) => ({
+      SalesOrderLineID: orderLine.content["m:properties"]["d:ID"],
+      QuantityDelivered: orderLine.content["m:properties"]["d:Quantity"],
+    }));
 
   await Promise.all([
     fetchExactAPI("POST", "/salesorder/GoodsDeliveries", {
       TrackingNumber,
       ShippingMethod: shippingMethodID,
-      DeliveryDate: currentAmsterdamTime,
-      GoodsDeliveryLines: orderLinesEntries.map((orderLine) => ({
-        SalesOrderLineID: orderLine.content["m:properties"]["d:ID"],
-        QuantityDelivered: orderLine.content["m:properties"]["d:Quantity"],
-      })),
+      DeliveryDate,
+      GoodsDeliveryLines,
     }),
     fetchExactAPI("PUT", `/salesorder/SalesOrders(guid'${orderID}')`, {
-      DeliveryDate: currentAmsterdamTime,
+      DeliveryDate,
     }),
   ]);
 
