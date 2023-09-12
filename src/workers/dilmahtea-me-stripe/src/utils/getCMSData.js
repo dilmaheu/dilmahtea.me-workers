12fb95fb9d1e4271182dda1b427c6430e8f66897
RafidMuhymin <rafidmuhymin@gmail.com>
2023-09-13 05:55:33 +0600

    fixed referer & origin headers null in production

--- src/workers/dilmahtea-me-stripe/src/utils/getCMSData.js
+++ src/workers/dilmahtea-me-stripe/src/utils/getCMSData.js
@@ -1,18 +1,10 @@
-export default async function getCMSData(request, env) {
-  const origin = (() => {
-    // check if an alternative origin exists
-    if (env.ALTERNATIVE_ORIGIN) {
-      const { hostname } = new URL(request.headers.get("referer"));
+export default async function getCMSData(origin_url, env) {
+  const { hostname, origin } = new URL(origin_url);
 
-      if (hostname === "localhost") {
-        return env.ALTERNATIVE_ORIGIN;
-      }
-    } else {
-      return request.headers.get("Origin");
-    }
-  })();
-
-  const validationDatasetURL = origin + "/db/validation-dataset.json";
+  const validationDatasetURL =
+    hostname === "localhost"
+      ? env.ALTERNATIVE_ORIGIN
+      : origin + "/db/validation-dataset.json";
 
   const CMSData = await fetch(validationDatasetURL).then((res) => res.json());
 
