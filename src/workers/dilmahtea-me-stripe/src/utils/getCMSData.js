85c8938d04429906966969979cb69564faf53025
RafidMuhymin <rafidmuhymin@gmail.com>
2023-09-15 19:13:32 +0600

    validate checkout creation request origin

--- src/workers/dilmahtea-me-stripe/src/utils/getCMSData.js
+++ src/workers/dilmahtea-me-stripe/src/utils/getCMSData.js
@@ -1,9 +1,26 @@
-export default async function getCMSData(origin_url, env) {
-  const { hostname, origin } = new URL(origin_url);
+// @ts-check
 
-  const validationDatasetURL =
-    (hostname === "localhost" ? env.ALTERNATIVE_ORIGIN : origin) +
-    "/db/validation-dataset.json";
+import { z } from "zod";
+
+export default async function getCMSData(origin_url, env) {
+  const originURL = new URL(origin_url);
+
+  const origin =
+    originURL.hostname === "localhost" ? env.ORIGIN : originURL.origin;
+
+  const OriginSchema = z
+    .string()
+    .regex(
+      new RegExp(
+        "^" + (env.CF_PAGES_DOMAIN?.replace("*", "[^.]+") || env.ORIGIN) + "$",
+      ),
+    );
+
+  if (!OriginSchema.safeParse(origin).success) {
+    throw new Error("Invalid origin");
+  }
+
+  const validationDatasetURL = origin + "/db/validation-dataset.json";
 
   const CMSData = await fetch(validationDatasetURL).then((res) => res.json());
 
