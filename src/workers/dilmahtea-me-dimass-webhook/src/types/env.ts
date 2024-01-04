c02c585271134969ff97f9d7ad71b727486440f6
RafidMuhymin <rafidmuhymin@gmail.com>
2024-01-05 02:17:42 +0600

    removed unused secret

--- src/workers/dilmahtea-me-dimass-webhook/src/types/env.ts
+++ src/workers/dilmahtea-me-dimass-webhook/src/types/env.ts
@@ -19,7 +19,6 @@ export interface ENV {
   DIMASS_API_SECRET: string;
   DIMASS_API_ENDPOINT: string;
   DIMASS_WEBHOOK_SECRET: string;
-  DIMASS_TEMP_WEBHOOK_SECRET: string;
 
   // STRAPI
   STRAPI_ACCESS_TOKEN: string;
